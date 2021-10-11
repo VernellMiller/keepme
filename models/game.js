97f73adb5615fcf60ea0a52ab8c5a808b3a84def
vernellmiller <nezeey@gmail.com>
2021-10-11 17:17:07 -0400

    wip: adds game schema and exports game

---
+++ models/game.js
@@ -0,0 +1,14 @@
+// const mongoose = ("mongoose");
+
+// const gameSchema = new mongoose.Schema({
+//         teamName: {type: String, required: true},
+//         teamCaptain: {type: String},
+//         teamColor: {type: String, required: true},
+//         division: {type: Number},
+//         gameTime: {type: Number, required: true},
+//         gameField: {type: String, required: true},
+// });
+
+// const Game = mongoose.model("Game", bookSchema)
+
+// module.exports = Game
